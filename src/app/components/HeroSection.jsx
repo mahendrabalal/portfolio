43b65749f6a86134df773848a6902d78b2e971bd
mahendrabalal <152038622+mahendrabalal@users.noreply.github.com>
2024-09-20 01:28:47 +0100

    changed profile picture

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -57,7 +57,7 @@ const HeroSection = () => {
         >
          <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[500px] relative">
   <Image
-    src="/images/mern.webp"
+    src="/images/gen.png"
     alt="hero-image"
     className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
     width={400}
